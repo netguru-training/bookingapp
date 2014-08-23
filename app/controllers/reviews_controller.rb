fea5aba08ca841d35e96c84114364fc826c0e0a8
Konrad <konrad.zwoinski@gmail.com>
2014-08-23 11:19:49 +0200

    Decent exposure for app controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,20 @@
 class ReviewsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new 
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def destroy  
+  end
 end
