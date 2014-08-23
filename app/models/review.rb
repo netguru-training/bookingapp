eaae5a14e087f76d52cebe61081f0d9a5fd71270
dominik <tomczakdominik@gmail.com>
2014-08-23 11:38:08 +0200

    reviews custom validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,12 @@ class Review < ActiveRecord::Base
   validates_presence_of :content, :rating, :place_id, :user_id
   validates_uniqueness_of :place_id, scope: :user_id
   validates_numericality_of :place_id, :user_id
+  validate :review_author_is_not_place_author
+
+  def review_author_is_not_place_author
+    if Place.find(place_id).user_id == user_id
+      errors.add(:base, "can't review your own place")
+    end
+  end
 
 end
