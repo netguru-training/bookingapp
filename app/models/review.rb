160c5718ce08443b5379dfeaca52adff32caf587
dominik <tomczakdominik@gmail.com>
2014-08-23 11:22:28 +0200

    review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,6 @@
 class Review < ActiveRecord::Base
+  validates_presence_of :content, :rating, :place_id, :user_id
+  validates_uniqueness_of :place_id, scope: :user_id
+  validates_numericality_of :place_id, :user_id
+
 end
