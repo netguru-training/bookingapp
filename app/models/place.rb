a5e075acb5521fe61dd981ee8020bf8209d2973b
jacobi007 <jk.kubiak@gmail.com>
2014-08-23 12:14:28 +0200

    fix has_many relations and add seed data

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,8 @@
 class Place < ActiveRecord::Base
   belongs_to :user
   has_many :reviews
-  has_many :reservations
-  
-  validates :price, :name, :description, presence: true 
-  
+  has_many :bookings
+
+  validates :price, :name, :description, presence: true
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete_all
+Place.delete_all
+Booking.delete_all
+
+10.times do |n|
+  User.create(email: "test#{n}@asd.com",
+              password: 'asdasdasd',
+              password_confirmation: 'asdasdasd')
+end
+
+15.times do |n|
+  Place.create(name: "Netguru Room #{n}",
+               price: rand(100),
+               description: 'cool place',
+               user_id: User.order("RANDOM()").first.id)
+end
+
+15.times do |n|
+  User.order("RANDOM()").first.bookings.create(place_id: Place.order("RANDOM()").first.id,
+                                               date_from: Time.now + rand(15).days,
+                                               date_to: Time.now + rand(15).days + 15.days)
+end
