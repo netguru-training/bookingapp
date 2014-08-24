cb571358912fd0b636b457162702bbe288d550dc
jacobi007 <jk.kubiak@gmail.com>
2014-08-24 09:48:28 +0200

    finish configuring signup with facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,10 @@ class User < ActiveRecord::Base
   def is_owner?(booking)
     booking.user == self
   end
+
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_create do |user|
+    user.email = auth.info.email
+    user.password = Devise.friendly_token[0,20]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   root 'home#index'
   # root to: "users#new"
   # Add Index and show views for bookings
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_scope :user do
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
 
   resources :home, only: :index
   resources :dashboard, only: :index
@@ -10,7 +13,6 @@ Rails.application.routes.draw do
   resources :places do
     resources :reviews
   end
-
 
   resources :search
   resources :bookings do
